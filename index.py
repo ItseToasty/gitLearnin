39e4ace25b549fddc19665b8573550f0b9aaebc4
Kayshawn Williams <kayshawnw@cpp.edu>
2025-08-28 15:21:16 -0700

    Initial commit

---
+++ index.py
@@ -0,0 +1,10 @@
+import time
+def worldDestroyer():
+    print("Destroying world...")
+    time.sleep(1)
+    print("Destroying world...")
+    time.sleep(1)
+    print("World destroyed!")
+
+worldDestroyer()
+'!'
