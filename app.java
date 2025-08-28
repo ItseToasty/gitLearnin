c94ff34c35bf80dd9bdc4242d5ea64450fdc2e02
Kayshawn Williams <kayshawnw@cpp.edu>
2025-08-28 15:32:00 -0700

    Changed app.js to app.java

---
+++ app.java
@@ -0,0 +1,8 @@
+public Class Endtime;
+    public static void main(String[] args) {
+        worldDestroyer();
+    }
+    public static void worldDestroyer(){
+        System.out.println("Destroying world");
+        System.out.print("Destroyed!");
+    }
